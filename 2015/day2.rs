28616ff619bae6eacc5b2e6d16a31bc6a0004f21
Atul Bhatt <atulbhatt61@gmail.com>
2023-05-10 21:04:04 +0530

    day2 2015

---
+++ 2015/day2.rs
@@ -0,0 +1,28 @@
+// Calculate total wrapping paper to buy
+use std::fs;
+use std::cmp::min;
+
+fn main() {
+    let input_dims = fs::read_to_string("input/day2.txt")
+        .expect("Cannot read file.");
+    let mut wrapping_paper_size = 0;
+
+    for dimensions in input_dims.lines() {
+        let dims_vec: Vec<&str> = dimensions.splitn(3, "x").collect();
+
+        // parse the &str values i32
+        let (l, w, h) = (dims_vec[0].to_string().parse::<i32>().unwrap(),
+            dims_vec[1].to_string().parse::<i32>().unwrap(),
+            dims_vec[2].to_string().parse::<i32>().unwrap()
+        );
+
+        wrapping_paper_size += 2 * l * w;
+        wrapping_paper_size += 2 * w * h;
+        wrapping_paper_size += 2 * h * l;
+
+        // smallest side
+        wrapping_paper_size += min(h*l, min(l*w, w*h));
+    }
+
+    println!("{wrapping_paper_size}");
+}
