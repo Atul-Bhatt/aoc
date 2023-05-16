efe060e17a26d303ae3e36056cf96dcee7dd04ae
Atul Bhatt <atulbhatt61@gmail.com>
2023-05-16 13:52:22 +0530

    day 4, part 2, 2015

--- 2015/day4.rs
+++ 2015/day4.rs
@@ -16,6 +16,20 @@ fn main() {
 
         target += 1;
     }
+
+    target = 0;
+
+    loop {
+        input.push_str(target.to_string().as_str());
+        let digest = md5::compute(input.clone());
+        
+        if format!("{:x}", digest).starts_with("000000") {
+            println!("{:x} {input}", digest);
+            break;
+        }
+
+        target += 1;
+    }
     
     println!("{target}");
 }
