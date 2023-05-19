d0990e0b4f278636b5b49f4ea249943a7a623b03
Atul Bhatt <atulbhatt61@gmail.com>
2023-05-19 13:33:00 +0530

    day 5, part 2, 2015

--- 2015/day5.rs
+++ 2015/day5.rs
@@ -7,7 +7,7 @@ fn main() {
     let mut nice_count = 0;
 
     for line in input.lines() {
-        if contains_vowels(&line) && contains_double(&line) && !contains_bad(&line) {
+        if contains_double(&line) {
             println!("{line}");
             nice_count += 1;
            }
@@ -33,12 +33,37 @@ fn contains_vowels(input: &str) -> bool {
 }
 
 fn contains_double(input: &str) -> bool {
+    let mut occurence_set = std::collections::HashSet::new();
+    let mut skip = false;
+
     for (idx, c) in input.clone().chars().enumerate() {
         if idx == 0 {
             continue;
         }
 
+        if skip == true {
+            skip = false;
+            continue;
+        }
+
         if c == input.as_bytes()[idx-1] as char {
+            if !occurence_set.insert(c) {
+                return true;
+            }
+            skip = true;
+        }
+    }
+
+    false
+}
+
+fn contains_repeat(input: &str) -> bool {
+    for (idx, c) in input.clone().chars().enumerate() {
+        if idx == 0 || idx == 1 {
+            continue;
+        }
+
+        if c == input.as_bytes()[idx-2] as char {
             return true;
         }
     }
