40c9bf423082c520f6eed67426beb6e8342c1ee0
Atul Bhatt <atulbhatt61@gmail.com>
2023-05-19 15:49:12 +0530

    day 6, part 1, finished.

--- 2015/day6.rs
+++ 2015/day6.rs
@@ -1,12 +1,12 @@
 use std::fs;
 
 struct Point {
-    x: i16,
-    y: i16,
+    x: usize,
+    y: usize,
 }
 
 impl Point {
-    fn new(x: i16, y: i16) -> Point {
+    fn new(x: usize, y: usize) -> Point {
         Point {x, y}
     }
 }
@@ -16,10 +16,12 @@ fn main() {
     let input = fs::read_to_string("input/day6.txt")
         .expect("Cannot read file");
 
+    let mut grid = vec![[0; 1000]; 1000];
+
     for line in input.lines() {
         let line_vec: Vec<_> = line.split(" ").collect();
         let first_point: Point;
-        let second_point: Point; 
+        let second_point: Point;
 
         // Find out the instruction type
         match line_vec[0] {
@@ -28,19 +30,31 @@ fn main() {
                     "on" => {
                         let mut first_point_iter = line_vec[2].split(",");
                         let mut second_point_iter = line_vec[4].split(",");
-                        first_point = Point::new(first_point_iter.next().unwrap().parse::<i16>().unwrap(),
-                            first_point_iter.next().unwrap().parse::<i16>().unwrap());
-                        second_point = Point::new(second_point_iter.next().unwrap().parse::<i16>().unwrap(),
-                            second_point_iter.next().unwrap().parse::<i16>().unwrap());
+                        first_point = Point::new(first_point_iter.next().unwrap().parse::<usize>().unwrap(),
+                            first_point_iter.next().unwrap().parse::<usize>().unwrap());
+                        second_point = Point::new(second_point_iter.next().unwrap().parse::<usize>().unwrap(),
+                            second_point_iter.next().unwrap().parse::<usize>().unwrap());
+                        
+                        for i in first_point.x..=second_point.x {
+                            for j in first_point.y..=second_point.y {
+                                grid[i][j] = 1;
+                            }
+                        }
                     },
 
                     "off" => {
                         let mut first_point_iter = line_vec[2].split(",");
                         let mut second_point_iter = line_vec[4].split(",");
-                        first_point = Point::new(first_point_iter.next().unwrap().parse::<i16>().unwrap(),
-                            first_point_iter.next().unwrap().parse::<i16>().unwrap());
-                        second_point = Point::new(second_point_iter.next().unwrap().parse::<i16>().unwrap(),
-                            second_point_iter.next().unwrap().parse::<i16>().unwrap());
+                        first_point = Point::new(first_point_iter.next().unwrap().parse::<usize>().unwrap(),
+                            first_point_iter.next().unwrap().parse::<usize>().unwrap());
+                        second_point = Point::new(second_point_iter.next().unwrap().parse::<usize>().unwrap(),
+                            second_point_iter.next().unwrap().parse::<usize>().unwrap());
+
+                        for i in first_point.x..=second_point.x {
+                            for j in first_point.y..=second_point.y {
+                                grid[i][j] = 0;
+                            }
+                        }
                     },
 
                     _ => { panic!("Wrong Instruction Type") }
@@ -50,14 +64,36 @@ fn main() {
             "toggle" => {
                 let mut first_point_iter = line_vec[1].split(",");
                 let mut second_point_iter = line_vec[3].split(",");
-                first_point = Point::new(first_point_iter.next().unwrap().parse::<i16>().unwrap(),
-                    first_point_iter.next().unwrap().parse::<i16>().unwrap());
-                second_point = Point::new(second_point_iter.next().unwrap().parse::<i16>().unwrap(),
-                    second_point_iter.next().unwrap().parse::<i16>().unwrap());
+                first_point = Point::new(first_point_iter.next().unwrap().parse::<usize>().unwrap(),
+                    first_point_iter.next().unwrap().parse::<usize>().unwrap());
+                second_point = Point::new(second_point_iter.next().unwrap().parse::<usize>().unwrap(),
+                    second_point_iter.next().unwrap().parse::<usize>().unwrap());
+                
+                for i in first_point.x..=second_point.x {
+                    for j in first_point.y..=second_point.y {
+                        if grid[i][j] == 1 {
+                            grid[i][j] = 0;
+                        } else  {
+                            grid[i][j] = 1;
+                        }
+                    }
+                }
             },
 
             _ => panic!("Wrong Instruction Type "),
         }
-    }
-}
 
+        // println!("{},{} {},{}", first_point.x, first_point.y, second_point.x, second_point.y);
+    }
+    let mut light_count = 0;
+
+    for i in 0..1000 {
+        for j in 0..1000 {
+            if grid[i][j] == 1 {
+                light_count += 1;
+            }
+        }
+    }
+
+    println!("{light_count}");
+}
