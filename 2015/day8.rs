363810f850f217d021fe35006a5808beb59c4a0b
Atul Bhatt <atulbhatt61@gmail.com>
2023-05-25 18:38:36 +0530

    day 8, part 2, 2015

--- 2015/day8.rs
+++ 2015/day8.rs
@@ -4,27 +4,17 @@ fn main() {
     let input = fs::read_to_string("input/day8.txt")
         .expect("Cannot read file.");
 
-    let mut final_count = 0;
+    let mut final_count: i32 = 0;
 
     for line in input.lines() {
-        final_count += line.trim().len();
-        let mut str_literal_count = line.trim().len() - 2;
-        let mut is_slash = false;
+        final_count += (line.trim().len() + 4) as i32;
+        let str_literal_count: i32 = (line.trim().len() + 2) as i32;
 
-        for (idx, c) in line.chars().enumerate() {
-            if is_slash {
-                is_slash = false;
-                continue;
-            }
-            if c == '\\' {
-                match line.as_bytes()[idx+1] as char {
-                    '\\' => {
-                        is_slash = true;
-                        str_literal_count -= 1
-                    }, 
-                    'x' => str_literal_count -= 3,
-                    _ => str_literal_count -= 1,
-                }
+        for c in line.chars() {
+            match c {
+                '"' => final_count += 1,
+                '\\' => final_count += 1,
+                _ => {}
             }
         }
         final_count -= str_literal_count;
